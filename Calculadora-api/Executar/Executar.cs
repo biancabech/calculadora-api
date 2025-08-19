cbb732ac4c45a240ae5ddb2a1c339637217de00f
Daniel-Stickel <danielstickel21@gmail.com>
2025-08-18 21:01:58 -0300

    adicionando soma

--- Calculadora-api/Executar/Executar.cs
+++ Calculadora-api/Executar/Executar.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Calculadora_api.Operacoes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,19 @@ using System.Threading.Tasks;
 
 namespace Calculadora_api.Executar
 {
-    internal class Executar
+    internal  class Executar : Soma
     {
+        public Executar(string _invalido, double entrada1, double entrada2, double resultado) : base(_invalido, entrada1, entrada2, resultado)
+        {
+            _Invalido = _invalido;
+            Entrada1 = entrada1;
+            Entrada2 = entrada2;
+            Resultado = resultado;
+        }
 
+        public virtual void Somar()
+        {
+            Console.WriteLine(Resultado);
+        }
     }
 }
--- Calculadora-api/Operacoes/Soma.cs
+++ Calculadora-api/Operacoes/Soma.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace Calculadora_api.Operacoes
 {
-    internal class Soma
+    internal abstract class Soma
     {
+        public string _Invalido = "Digite apenas números";
+
+        public static double Entrada1 { get; set; }
+
+        public static double Entrada2 { get; set; }
+
+        public static double Resultado { get; set; }
+
+        public Soma(string _invalido, double entrada1, double entrada2, double resultado)
+        {
+            _Invalido = _invalido;
+            Entrada1 = entrada1;
+            Entrada2 = entrada2;
+            Resultado = resultado;
+        }
+
+        public override void Somar()
+        {
+            if (Entrada1 != null && Entrada2 != null)
+            {
+                Resultado = Entrada1 + Entrada2;
+            }
+            else
+            {
+                Console.WriteLine(_Invalido);
+            }
+
+        }
     }
 }
+
