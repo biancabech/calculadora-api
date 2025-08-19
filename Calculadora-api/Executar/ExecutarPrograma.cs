42fcbca9c7ad57052b517ea787d6eca278a7e937
Marcos Vinicius Peretro Júnior <peretro.junior7@gmail.com>
2025-08-18 21:18:31 -0300

    Alteracao de utilidades

--- Calculadora-api/Executar/ExecutarPrograma.cs
+++ Calculadora-api/Executar/ExecutarPrograma.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Calculadora_api.Operacoes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,9 +19,39 @@ namespace Calculadora_api.Executar
             Entrada1 = entrada1;
             Entrada2 = entrada2;
         }
-        static public void Executar()
+        public static void Executar()
         {
-            
+            bool continuar = false;
+            do
+            {
+                Console.WriteLine("insira a operação que deseja fazer ");
+                Console.WriteLine("+ : Adição");
+                Console.WriteLine("- : Subtração");
+                Console.WriteLine("* : Multiplicação");
+                Console.WriteLine("/ : Divisão");
+                string operacao = Console.ReadLine();
+
+                switch (operacao)
+                {
+                    case "+":
+
+                        break;
+                    case "-":
+                        Subtracao.Subtrair();
+                        break;
+                    case "*":
+
+                        break;
+                    case "/":
+
+                        break;
+                    default:
+                        Console.WriteLine("Valor inválido escolha novamente ");
+                        continuar = true;
+                        break;
+                }
+                continuar = false;
+            } while (continuar);
         }
 
         
--- Calculadora-api/Operacoes/Subtracao.cs
+++ Calculadora-api/Operacoes/Subtracao.cs
@@ -1,17 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Calculadora_api.Utils;
 
 namespace Calculadora_api.Operacoes
 {
     internal class Subtracao
     {
-        
         public static void Subtrair()
         {
+            List<double> listaNumeros = new List<double>();
             
+            Console.WriteLine("Deseja inserir quantos valores?");
+            int quantidadeValores = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < quantidadeValores; i++)
+            {
+                listaNumeros.Add(Utilidades.EscolherValor());
+            }
+
+            double resultado = listaNumeros[0];
+            foreach (double valor in listaNumeros.Skip(1))
+            {
+                resultado -= valor;
+            }
+
+            Console.WriteLine("Resultado: " + resultado);
+
         }
     }
 }
